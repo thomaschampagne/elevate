4b41ebe43e131c31860a47504c9e4c8c8c8e910d
Thomas Champagne <th.champagne@gmail.com>
2015-02-22 14:34:03 +0100

    Folder move

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -2,6 +2,6 @@ var env = {
     buildNumber: 17, // 0.4.7
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
-    debugMode: true, // Must be false in release
+    debugMode: false, // Must be false in release
     useActivityStreamCache: false // Must be true in release
 };
