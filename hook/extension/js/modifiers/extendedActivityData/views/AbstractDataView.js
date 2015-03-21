6c45cc8d551aa33dd735cb4be6996254ad854159
Thomas Champagne <th.champagne@gmail.com>
2015-03-21 17:49:38 +0100

    Label edit

--- hook/extension/js/modifiers/extendedActivityData/views/AbstractDataView.js
+++ hook/extension/js/modifiers/extendedActivityData/views/AbstractDataView.js
@@ -81,7 +81,7 @@ var AbstractDataView = Fiber.extend(function(base) {
 
             var labelsData = [];
             for (var zone in zones) {
-                var label = "Z" + (parseInt(zone) + 1) + ": " + (zones[zone].from * ratio).toFixed(1) + " - " + (zones[zone].to * ratio).toFixed(1) + " " + this.units;
+                var label = "Z" + (parseInt(zone) + 1) + " " + (zones[zone].from * ratio).toFixed(1) + " to " + (zones[zone].to * ratio).toFixed(1) + " " + this.units;
                 labelsData.push(label);
             }
 
