4e496887e733d56ea5ed798bcc2f1f526801cb94
Thomas Champagne <th.champagne@gmail.com>
2015-03-26 01:25:59 +0100

    Remove temporary message for new extended stats

--- hook/extension/js/modifiers/extendedActivityData/AbstractExtendedActivityDataModifier.js
+++ hook/extension/js/modifiers/extendedActivityData/AbstractExtendedActivityDataModifier.js
@@ -23,14 +23,7 @@ var AbstractExtendedActivityDataModifier = Fiber.extend(function(base) {
 
 
         modify: function() {
-
-            // Temporary message for new extended stats
-            if (!StorageManager.getCookie('hideTmpMessageExtendedStats')) {
-                this.content += '<p id="tmpMessageExtendedStats" style="padding: 5px;background: #FFF0A0;font-size: 12px;color: rgb(103, 103, 103);">This extended stats panel is NEW ! ';
-                this.content += 'So feel free to report problems <a target="_blank" href="https://chrome.google.com/webstore/detail/stravaplus/dhiaggccakkgdfcadnklkbljcgicpckn/support">HERE</a> (don\'t forget activities links in report).<br/>';
-                this.content += 'Follow feature updates through <a target="_blank" href="https://twitter.com/champagnethomas">@champagnethomas</a>.<br/>This message will disappear when stravaplus users have acclaimed this new feature (approximately few days...)<br/><br/><a onclick="javascript:jQuery(\'#tmpMessageExtendedStats\').hide(); document.cookie=\'hideTmpMessageExtendedStats=true\';">Never show this again</a></p>';
-            }
-
+            
             _.each(this.dataViews, function(view) {
                 // Append result of view.render() to this.content
                 view.render();
