63b8aa3e908db72be7046570540de9b878806406
Thomas Champagne <th.champagne@gmail.com>
2015-03-27 13:47:06 +0100

    Release 0.5.5 version and build number

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,8 +1,8 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 25, // 0.5.4
+    buildNumber: 26, // 0.5.5
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
-    useActivityStreamCache: false // Must be true in release
+    useActivityStreamCache: true // Must be true in release
 };
