e85cf223d7a9da4b7256f9015d388cb8ba2b6875
Dejan Kamenšek <kamensek@gmail.com>
2015-04-01 00:17:25 +0200

    Changes to enable HR analysis for other activities

--- hook/extension/js/modifiers/extendedActivityData/GenericExtendedActivityDataModifier.js
+++ hook/extension/js/modifiers/extendedActivityData/GenericExtendedActivityDataModifier.js
@@ -8,6 +8,27 @@ var GenericExtendedActivityDataModifier = AbstractExtendedActivityDataModifier.e
 
         modify: function() {
             base.modify.call(this);  // Super call
+        },
+        
+        setDataViewsNeeded: function() {
+            base.setDataViewsNeeded.call(this);
+
+            // Speed view
+            if (this.analysisData_.speedData && this.userSettings_.displayAdvancedSpeedData) {
+                var measurementPreference = currentAthlete.get('measurement_preference');
+                var units = (measurementPreference == 'meters') ? 'kph' : 'mph';
+                var speedDataView = new SpeedDataView(this.analysisData_.speedData, units);
+                speedDataView.setAppResources(this.appResources_);
+                speedDataView.setIsAuthorOfViewedActivity(this.isAuthorOfViewedActivity);
+                this.dataViews.push(speedDataView);
+            }
+
+            if (this.analysisData_.cadenceData && this.userSettings_.displayCadenceData) {
+                var cyclingCadenceDataView = new CyclingCadenceDataView(this.analysisData_.cadenceData, 'rpm');
+                cyclingCadenceDataView.setAppResources(this.appResources_);
+                cyclingCadenceDataView.setIsAuthorOfViewedActivity(this.isAuthorOfViewedActivity);
+                this.dataViews.push(cyclingCadenceDataView);
+            }
         }
     }
 });
