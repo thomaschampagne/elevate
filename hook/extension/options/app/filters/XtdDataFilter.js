e2468c0b3a7b06305409f159ba501fe62ab73420
Thomas Champagne <th.champagne@gmail.com>
2015-04-02 21:05:40 +0200

    #70 clean

--- hook/extension/options/app/filters/XtdDataFilter.js
+++ hook/extension/options/app/filters/XtdDataFilter.js
@@ -8,29 +8,13 @@ app.filter('xtdDataFilter', function() {
     };
 
     return function(value, type) {
-
         var result = '';
-
         if (type === 'speed') {
-
             var mph = value * 0.621371192;
             result = ' = ' + mph.toFixed(2) + ' mph';
-
         } else if (type === 'pace') {
-
-            var seconds = value;
-            // console.log();
-
-            //(speed === 0) ? 'infinite' : parseInt((1 / speed) * 60 * 60);
-
-            result = formatTime(seconds) + '/km | ' + formatTime(seconds / 0.621371192) + '/mi';
+            result = formatTime(value) + '/km | ' + formatTime(value / 0.621371192) + '/mi';
         };
-
         return result;
     };
-
-
-    // TODO give kph and mph for speed
-
-    // TODO give time/km or time/mi for pace
 });
