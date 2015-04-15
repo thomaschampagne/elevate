1e8228f604345564caa8f743c31e7f7fb08574b1
Thomas Champagne <th.champagne@gmail.com>
2015-04-15 19:20:27 +0200

    Release 0.6.0

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
-    preview: true, // Must be false in release
-    buildNumber: 27, // 0.6.0-rc1
+    preview: false, // Must be false in release
+    buildNumber: 28, // 0.6.0
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
