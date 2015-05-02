457c2bff9ce1ddac245418248b3b6abd1ed4786e
Dejan Kamenšek <kamensek@gmail.com>
2015-05-02 02:04:27 +0200

    0.6.0.2

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 601, // 0.6.0.1
+    buildNumber: 602, // 0.6.0.2
     analyticsTrackingID: 'UA-62527089-1', // my Google Analytics Code
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
