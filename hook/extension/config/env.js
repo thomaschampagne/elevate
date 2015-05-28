8aa326870dee4e3ea5f7df54aa646dcf857b14aa
Thomas Champagne <th.champagne@gmail.com>
2015-05-28 12:20:09 +0200

    build 33 is 0.7.0 not 0.6.5

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 33, // 0.6.5
+    buildNumber: 33, // 0.7.0
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
