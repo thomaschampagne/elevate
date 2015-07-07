cedefdc26fbfb73613cf97c2a650a473fe288c93
tazmanska@gmail.com <tazmanska@gmail.com>
2015-07-07 22:54:42 +0200

    Activity segments time comparison to KOM and previous PR

--- hook/extension/js/StravistiX.js
+++ hook/extension/js/StravistiX.js
@@ -70,6 +70,7 @@ StravistiX.prototype = {
         this.handleExtendedActivityData_();
         this.handleNearbySegments_();
         this.handleActivityBikeOdo_();
+        this.handleActivitySegmentTimeComparison_();
 
         // Run
         this.handleRunningGradeAdjustedPace_();
@@ -494,6 +495,36 @@ StravistiX.prototype = {
 
         }.bind(this));
     },
+    
+    /**
+     *
+     */
+    handleActivitySegmentTimeComparison_: function() {
+
+        // Test where are on an activity...
+        if (!window.location.pathname.match(/^\/activities/)) {
+            return;
+        }
+
+        if (_.isUndefined(window.pageView)) {
+            return;
+        }
+
+        // Only cycling is supported
+        if (window.pageView.activity().attributes.type != "Ride") {
+            return;
+        }
+        
+        // Only for own activities
+        if (this.athleteId_ != this.athleteIdAuthorOfActivity_) {
+            return;
+        }
+
+        if (env.debugMode) console.log("Execute handleActivitySegmentTimeComparison_()");
+            
+        var activitySegmentTimeComparisonModifier = new ActivitySegmentTimeComparisonModifier();
+        activitySegmentTimeComparisonModifier.modify();
+    },    
 
     /**
      *
