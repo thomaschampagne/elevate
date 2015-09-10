b58e8686e497c7e33dc531cc9a3d7f8dc17784a2
Thomas Champagne <th.champagne@gmail.com>
2015-09-11 01:53:05 +0200

    #117 Support highlighted path on activity loaded with segment details

--- hook/extension/js/modifiers/GoogleMapsComeBackModifier.js
+++ hook/extension/js/modifiers/GoogleMapsComeBackModifier.js
@@ -20,40 +20,83 @@ GoogleMapsComeBackModifier.prototype = {
 
         $(window).bind('gMapsLoaded', this.fetchPathFromStream(this.activityId));
         this.getGoogleMapsApi();
+
+/*
+        $('[data-segment-effort-id]').click(function () {
+            console.log(''toot'');
+            console.log(this);
+        });
+*/
     },
 
     fetchPathFromStream: function(activityId) {
 
-        var url = "/activities/" + activityId + "/streams?stream_types[]=latlng";
+        var streamPathUrl = "/activities/" + activityId + "/streams?stream_types[]=latlng";
 
-        $.ajax(url).done(function(jsonResponse) {
+        $.ajax(streamPathUrl).done(function(jsonResponse) {
 
-            this.displayGoogleMapWithPath(jsonResponse.latlng);
+            var effortId = (window.location.pathname.split('/')[4] || window.location.hash.replace('#', '')) || false;
+
+            if (effortId) {
+
+                // Get effortId bounds
+                var segmentInfosResponse;
+                $.ajax({
+                    url: '/segment_efforts/' + effortId,
+                    type: 'GET',
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    },
+                    dataType: 'json',
+                    success: function(xhrResponseText) {
+                        segmentInfosResponse = xhrResponseText;
+                    },
+                    error: function(err) {
+                        console.error(err);
+                    }
+                }).then(function() {
+
+                    // Call Activity Processor with bounds
+                    if (!segmentInfosResponse.start_index && segmentInfosResponse.end_index) {
+                        console.error('No start_index end_index found for');
+                    }
+
+                    // Slice latlong array
+                    this.displayGoogleMapWithPath(
+                        jsonResponse.latlng, [segmentInfosResponse.start_index, segmentInfosResponse.end_index]
+                    );
+
+                }.bind(this));
+
+
+            } else {
+                this.displayGoogleMapWithPath(jsonResponse.latlng);
+            }
 
         }.bind(this));
 
     },
 
-    displayGoogleMapWithPath: function(pathArray) {
+    displayGoogleMapWithPath: function(mainPathArray, highlightFromTo) {
 
         var html = '<div style="padding-bottom:10px;"><div style="height:350px;width:100%;" id="gmaps_canvas"></div></div>';
 
         $('#map-canvas').before(html).each(function() {
 
-            var map = new google.maps.Map(document.getElementById("gmaps_canvas"), {
+            this.map = new google.maps.Map(document.getElementById("gmaps_canvas"), {
                 mapTypeId: google.maps.MapTypeId.TERRAIN
             });
 
             var points = [];
             var bounds = new google.maps.LatLngBounds();
 
-            _.each(pathArray, function(position) {
+            _.each(mainPathArray, function(position) {
                 var p = new google.maps.LatLng(position[0], position[1]);
                 points.push(p);
                 bounds.extend(p);
             });
 
-            var poly = new google.maps.Polyline({
+            var mainPathPoly = new google.maps.Polyline({
                 // use your own style here
                 path: points,
                 strokeColor: "#FF0000",
@@ -61,10 +104,34 @@ GoogleMapsComeBackModifier.prototype = {
                 strokeWeight: 4
             });
 
-            poly.setMap(map);
+            // Set path to map
+            mainPathPoly.setMap(this.map);
 
             // fit bounds to track
-            map.fitBounds(bounds);
+            this.map.fitBounds(bounds);
+
+            if (highlightFromTo) {
+
+                var secondPathPoly = new google.maps.Polyline({
+                    path: points.slice(highlightFromTo[0], highlightFromTo[1]),
+                    strokeColor: "#105cb6",
+                    strokeOpacity: 1,
+                    strokeWeight: 4
+                });
+
+                // Erase bounds and computed new ones with highlighted path
+                bounds = new google.maps.LatLngBounds();
+                _.each(mainPathArray.slice(highlightFromTo[0], highlightFromTo[1]), function(position) {
+                    var p = new google.maps.LatLng(position[0], position[1]);
+                    bounds.extend(p);
+                });
+
+                // Update with new bounds from highlighted path
+                this.map.fitBounds(bounds);
+
+                // Apply new poly line
+                secondPathPoly.setMap(this.map);
+            }
 
         }.bind(this));
 
