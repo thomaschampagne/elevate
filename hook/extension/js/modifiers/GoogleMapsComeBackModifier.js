169729f0c7c24710e66b01b11fad76f9b7d6e7b0
Thomas Champagne <th.champagne@gmail.com>
2015-10-07 01:42:41 +0200

    #117 Change message to 'Disable this feature in settings'

--- hook/extension/js/modifiers/GoogleMapsComeBackModifier.js
+++ hook/extension/js/modifiers/GoogleMapsComeBackModifier.js
@@ -246,7 +246,7 @@ GoogleMapsComeBackModifier.prototype = {
             window.innerHeight * 0.9
         ];
 
-        var html = '<div style="padding-bottom:10px; text-align:center;"><div style="height:' + mapSize[1] + 'px;width:' + mapSize[0] + 'px;" id="gmaps_canvas"></div><a target="_blank" href="' + this.appResources.settingsLink + '#/commonSettings?viewOptionHelperId=reviveGoogleMaps">Disable this feature in settings</a></div>';
+        var html = '<div style="padding-bottom:10px; text-align:center;"><div style="height:' + mapSize[1] + 'px;width:' + mapSize[0] + 'px;" id="gmaps_canvas"></div><a target="_blank" href="' + this.appResources.settingsLink + '#/commonSettings?viewOptionHelperId=reviveGoogleMaps">Go to extension settings if you want to disable google maps buttons</a></div>';
 
         $.fancybox(html, {
             'autoScale': true,
