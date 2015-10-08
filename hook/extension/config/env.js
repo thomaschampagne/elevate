276be2710fc837dbaa51c3806afb5db393bc9109
Thomas Champagne <th.champagne@gmail.com>
2015-10-08 20:56:27 +0200

    Set activity stream cache to enabled. Disabled in latest release :/

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -4,5 +4,5 @@ var env = {
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
-    useActivityStreamCache: false // Must be true in release
+    useActivityStreamCache: true // Must be true in release
 };
