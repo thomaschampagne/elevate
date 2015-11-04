fdf19bf1ae4c00550857a7a3fb28eff42747502c
Thomas Champagne <th.champagne@gmail.com>
2015-11-04 01:34:31 +0100

    #130 Helper aggregation

--- hook/extension/options/app/services/Helper.js
+++ hook/extension/options/app/services/Helper.js
@@ -6,12 +6,19 @@ app.factory('AvoidInputKeys', function() {
     };
 });
 
-// app.config(['$provide', function($provide) {
+app.factory('Notifier', function($modal) {
 
-//     $provide.factory('Hash', function() {
-//     	return 'toto;';
-//     });
-// }]);
+    return function(title, content) {
+
+        content = '<div class="modal-header"><h3 class="modal-title">' + title + '</h3></div><div class="modal-body">' + content + '</div>'; //<div class="modal-footer"><button class="btn btn-default" data-ng-click="closeModal()">Close</button></div>';
+
+        var modalInstance = $modal.open({
+            template: content,
+            size: 'lg'
+        });
+    };
+});
+
 
 app.factory('Hash', function() {
 
