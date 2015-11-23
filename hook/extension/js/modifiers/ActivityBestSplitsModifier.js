d67ea9bd0c062d7822eb137ef3452c4db83f5c63
glandais <gabriel.landais@gmail.com>
2015-11-23 13:57:45 +0100

    Best elevation gain/drop in best splits
    Filtering of elevation done in ActivityBestSplitsModifier, should be
    done in ActivityProcessor

--- hook/extension/js/modifiers/ActivityBestSplitsModifier.js
+++ hook/extension/js/modifiers/ActivityBestSplitsModifier.js
@@ -67,6 +67,18 @@ ActivityBestSplitsModifier.prototype = {
             selectedSplitId,
             measurementPreference = currentAthlete ? currentAthlete.get('measurement_preference') : 'meters';
 
+        var filterData = function(data, distance, smoothing) {
+            if (data && distance) {
+                var result = [];
+                result[0] = data[0];
+                for (i = 1, max = data.length; i < max; i++) {
+                    result[i] = result[i-1] + (distance[i] - distance[i-1]) * (data[i] - result[i-1]) / smoothing;
+                }
+                return result;
+            }
+        };
+        this.activityJson.filteredAltitude = filterData(this.activityJson.altitude, this.activityJson.distance, 300);
+
         self.distanceUnit = (measurementPreference == 'meters') ? ActivityBestSplitsModifier.Units.Kilometers : ActivityBestSplitsModifier.Units.Miles;
 
         segments.find("h3.segments-header")
@@ -198,6 +210,8 @@ ActivityBestSplitsModifier.prototype = {
                             "<th style='text-align: center'>Rise HR</th>" +
                             "<th style='text-align: center'>Avg Power</th>" +
                             "<th style='text-align: center'>Avg Cadence</th>" +
+                            "<th style='text-align: center'>Elevation gain</th>" +
+                            "<th style='text-align: center'>Elevation drop</th>" +
                             "<th style='text-align: center'></th>" +
                             "</tr>" +
                             "</thead>" + 
@@ -269,6 +283,8 @@ ActivityBestSplitsModifier.prototype = {
                                    "<td class='value'><div id='split-" + split.id + "-rise-hr'></div></td>" +
                                    "<td class='value'><div id='split-" + split.id + "-avg-power'></div></td>" +
                                    "<td class='value'><div id='split-" + split.id + "-avg-cadence'></div></td>" +
+                                   "<td class='value'><div id='split-" + split.id + "-elevation-gain'></div></td>" +
+                                   "<td class='value'><div id='split-" + split.id + "-elevation-drop'></div></td>" +
                                    "<td><button class='compact minimal toggle-effort-visibility best-split-remove' data-split-id='" + split.id + "'>Remove</button></td>" +
                                    "</tr>");
         };
@@ -384,7 +400,7 @@ ActivityBestSplitsModifier.prototype = {
                                 avgSpeed,
                                 time,
                                 begin,
-                                end,      
+                                end,
                                 newSplitValue = function(value) {
                                     return {
                                         value: value || 0,
@@ -413,7 +429,9 @@ ActivityBestSplitsModifier.prototype = {
                                     dropHr: newDropRiseValue(),
                                     riseHr: newDropRiseValue(),
                                     avgPower: newSplitValue(),
-                                    avgCadence: newSplitValue()
+                                    avgCadence: newSplitValue(),
+                                    elevationGain: newSplitValue(),
+                                    elevationDrop: newSplitValue()
                                 },
                                 countSamples = function(value) {
                                     value.samples = value.end - value.begin + 1;
@@ -422,6 +440,30 @@ ActivityBestSplitsModifier.prototype = {
                                     var result = 0;
                                     for (; array && start <= end; start++) {
                                         result += array[start];
+                                    }
+                                    return result;
+                                },
+                                totalGainOfValues = function(start, end, array) {
+                                    var result = 0;
+                                    var previous = array[start++];
+                                    for (; array && start <= end; start++) {
+                                        var value = array[start];
+                                        if (previous < value) {
+                                            result += (value - previous);
+                                        }
+                                        previous = value;
+                                    }
+                                    return result;
+                                },
+                                totalDropOfValues = function(start, end, array, distance, smoothing) {
+                                    var result = 0;
+                                    var previous = array[start++];
+                                    for (; array && start <= end; start++) {
+                                        var value = array[start];
+                                        if (previous > value) {
+                                            result += (previous - value);
+                                        }
+                                        previous = value;
                                     }
                                     return result;
                                 },
@@ -543,6 +585,21 @@ ActivityBestSplitsModifier.prototype = {
                                         values.avgPower.end = end;
                                         values.avgPower.timeOrDistance = timeOrDistance;
                                     }
+
+                                    elevationGain = totalGainOfValues(begin, end, activityJson.filteredAltitude);
+                                    elevationDrop = totalDropOfValues(begin, end, activityJson.filteredAltitude);
+                                    if (elevationGain > values.elevationGain.value) {
+                                        values.elevationGain.value = elevationGain;
+                                        values.elevationGain.begin = begin;
+                                        values.elevationGain.end = end;
+                                        values.elevationGain.timeOrDistance = timeOrDistance;
+                                    }
+                                    if (elevationDrop > values.elevationDrop.value) {
+                                        values.elevationDrop.value = elevationDrop;
+                                        values.elevationDrop.begin = begin;
+                                        values.elevationDrop.end = end;
+                                        values.elevationDrop.timeOrDistance = timeOrDistance;
+                                    }
                                     
                                     avgSpeed = (distance / 1000) / (time / 60 / 60);
                                     if (avgSpeed > values.avgSpeed.value) {
@@ -652,6 +709,8 @@ ActivityBestSplitsModifier.prototype = {
                             countSamples(values.dropHr);
                             countSamples(values.riseHr);
                             countSamples(values.avgPower);
+                            countSamples(values.elevationGain);
+                            countSamples(values.elevationDrop);
                             countSamples(values.avgSpeed);
                             countSamples(values.distance);
                             countSamples(values.time);
@@ -737,6 +796,8 @@ ActivityBestSplitsModifier.prototype = {
                 setValue(splitId + "-drop-hr", value.dropHr, function(value) { return "-" + Helper.formatNumber(value.value, 0) + "bpm"; }, "n/a", formatTooltipDropRise);
                 setValue(splitId + "-rise-hr", value.riseHr, function(value) { return "+" + Helper.formatNumber(value.value, 0) + "bpm"; }, "n/a", formatTooltipDropRise);
                 setValue(splitId + "-avg-power", value.avgPower, function(value) { return Helper.formatNumber(value, 0) + "W"; }, "n/a", formatTooltip);
+                setValue(splitId + "-elevation-gain", value.elevationGain, function(value) { return Helper.formatNumber(value, 0) + "m"; }, "n/a", formatTooltip);
+                setValue(splitId + "-elevation-drop", value.elevationDrop, function(value) { return Helper.formatNumber(value, 0) + "m"; }, "n/a", formatTooltip);
                 setValue(splitId + "-avg-cadence", value.avgCadence, function(value) { return Helper.formatNumber(value, 0); }, "n/a", formatTooltip);
                 splitRow.find("td.value span.ajax-loading-image").remove();
             });
