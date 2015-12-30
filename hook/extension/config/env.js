8951bb91157dc964c797507501ab035eb45d0135
Thomas Champagne <th.champagne@gmail.com>
2015-12-30 17:01:01 +0100

    Hotfix 2.2.2

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 55, // 2.2.1
+    buildNumber: 56, // 2.2.2
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
