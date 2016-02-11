b0d1f8e2a468b4d4b86d43f516ef9a632e52e861
Thomas Champagne <th.champagne@gmail.com>
2016-02-11 01:37:27 +0100

    Update for version 3.1.0

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 59, // 3.0.1
+    buildNumber: 60, // 3.1.0
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
