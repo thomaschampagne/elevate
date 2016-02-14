01c518ed653797f2865803cfccc2df31bc3ca15c
glandais <gabriel.landais@gmail.com>
2016-02-14 21:20:34 +0100

    Best year, again!

--- hook/extension/js/modifiers/AthleteStatsModifier.js
+++ hook/extension/js/modifiers/AthleteStatsModifier.js
@@ -219,6 +219,8 @@ AthleteStatsModifier.prototype = {
                 '<li style="margin: 8px"><input id="asrdt1" type="radio" name="data-type" value="0" /><label style="display: inline" for="asrdt1">Activity count</label></li>' +
                 '<li style="margin: 8px"><input id="asrdt2" type="radio" name="data-type" value="2" /><label style="display: inline" for="asrdt2">Elevation</label></li>' +
                 '<li style="margin: 8px"><input id="asrdt3" type="radio" name="data-type" value="3" /><label style="display: inline" for="asrdt3">Time</label></li>' +
+                '<li style="margin: 8px"><input id="asrdt4" type="radio" name="data-type" value="4" /><label style="display: inline" for="asrdt4">Distance last year</label></li>' +
+                '<li style="margin: 8px"><input id="asrdt5" type="radio" name="data-type" value="5" /><label style="display: inline" for="asrdt5">Distance last 30d</label></li>' +
                 '</ul>' +
                 '<div style="margin-top: 20px;">Years:</div>' +
                 '<ul id="athleteStatChartYears"></ul>' +
