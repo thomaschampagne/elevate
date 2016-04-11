7c587b8071a977d27a2ce3f0960fd6333e9cbb38
Thomas Champagne <th.champagne@gmail.com>
2016-04-12 00:52:43 +0200

    Hotfix 3.3.1

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,6 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 66, // 3.3.0
+    buildNumber: 67, // 3.3.1
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
