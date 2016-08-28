714101c66c1bfaeea26def9e62bd95624044e7e9
Thomas Champagne <th.champagne@gmail.com>
2016-08-29 00:26:18 +0200

    #218 Remove useless build number

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,6 +1,5 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 77, // 3.10.0
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
