ddf9c8e62eb03a66f8dc0121fd1bda36cd1f6d6c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-08-29 13:19:02 +0200

    Turn debug mode on.

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -3,6 +3,6 @@ var env = {
     buildNumber: 78, // 3.10.1
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
-    debugMode: false, // Must be false in release
+    debugMode: true, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };
