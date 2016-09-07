77fbf930e201addb9cf1fcd846171bf2883a6856
Thomas Champagne <th.champagne@gmail.com>
2016-09-08 00:12:31 +0200

    Reduce height size of zones

--- hook/extension/options/app/filters/XtdDataFilter.js
+++ hook/extension/options/app/filters/XtdDataFilter.js
@@ -3,7 +3,7 @@
  */
 app.filter('xtdDataFilter', function() {
 
-    var formatTime = function (seconds) {
+    var formatTime = function(seconds) {
         return Helper.secondsToHHMMSS(seconds).replace('00:', '');
     };
 
@@ -13,7 +13,10 @@ app.filter('xtdDataFilter', function() {
             var mph = value * 0.621371192;
             result = mph.toFixed(2) + ' mph';
         } else if (type === 'pace') {
-            result = formatTime(value) + '/km or ' + formatTime(value / 0.621371192) + '/mi';
+            result = formatTime(value) + '/km | ' + formatTime(value / 0.621371192) + '/mi';
+        }
+        if (_.isEmpty(result)) {
+            return false;
         }
         return result;
     };
