c962849fcda1b92b9c110ba934464a1631b70c51
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-09-21 11:55:16 +0200

    Remove debugging code from develop branch, so that it matches upstream/develop.

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -2,6 +2,6 @@ var env = {
     preview: false, // Must be false in release
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
-    debugMode: true, // Must be false in release
+    debugMode: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };
