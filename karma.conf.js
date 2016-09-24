fb2060e8c023e8c49cba1745fdd944ca01952109
Thomas Champagne <th.champagne@gmail.com>
2016-09-24 21:32:30 +0200

    Using phantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         frameworks: ['jasmine'],
         files: [
             'dist/js/processors/*.js',
