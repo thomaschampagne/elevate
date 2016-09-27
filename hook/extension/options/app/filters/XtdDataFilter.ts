5c92e635b5a6b4f2bf1c9dc60ef2679dbcb055d5
Thomas Champagne <th.champagne@gmail.com>
2016-09-28 00:45:11 +0200

    #279 Migrate angular filters to TypeScript

--- hook/extension/options/app/filters/XtdDataFilter.ts
+++ hook/extension/options/app/filters/XtdDataFilter.ts
@@ -1,22 +1,22 @@
 /**
  * Return the right preview value when using custom xtd zones along units type
  */
-app.filter('xtdDataFilter', function() {
+app.filter('xtdDataFilter', () => {
 
-    var formatTime = function(seconds) {
+    let formatTime: Function = (seconds: number) => {
         return Helper.secondsToHHMMSS(seconds, true);
     };
 
-    return function(value, type) {
-        var result = '';
+    return (value: number, type: string) => {
+        let result: string = '';
         if (type === 'speed') {
-            var mph = value * 0.621371192;
+            let mph: number = value * 0.621371192;
             result = mph.toFixed(2) + ' mph';
         } else if (type === 'pace') {
             result = formatTime(value) + '/km | ' + formatTime(value / 0.621371192) + '/mi';
         }
         if (_.isEmpty(result)) {
-            return false;
+            return null;
         }
         return result;
     };
