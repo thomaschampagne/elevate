483ff771dec73fb6b7ee9023ca0e422e6d6a5ac8
Thomas Champagne <th.champagne@gmail.com>
2017-01-01 17:01:28 +0100

    Form and fatigue colors edit

--- plugin/options/app/App.ts
+++ plugin/options/app/App.ts
@@ -15,8 +15,8 @@ interface IColors {
 let $colors: IColors = {
     strava: '#e94e1b',
     ctl: '#e94e1b',
-    atl: '#5f5f5f',
-    tsb: '#a7a7a7',
+    atl: '#4a4a4a',
+    tsb: '#adadad',
     lightGrey: '#eeeeee',
     midGrey: '#4e4e4e',
     strongGrey: '#2f2f2f'
