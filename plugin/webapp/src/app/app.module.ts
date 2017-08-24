3d996e6d75c6269b9ecb8f652c4e8509bc063fb8
Thomas Champagne <th.champagne@gmail.com>
2017-08-24 23:10:56 +0200

    Implementing basic navigation with router

--- plugin/webapp/src/app/app.module.ts
+++ plugin/webapp/src/app/app.module.ts
@@ -1,35 +1,27 @@
 import "hammerjs";
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MaterialModule} from "./material.module";
-import {FlexLayoutModule} from "@angular/flex-layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./shared/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import {AppComponent} from "./app.component";
+import { AppComponent } from "./app.component";
+import { AthleteSettingsComponent } from './athlete-settings/athlete-settings.component';
+import { CommonSettingsComponent } from './common-settings/common-settings.component';
+import { RoutingModule } from "./shared/routing.module";
 
-/*
-const appRoutes: Routes = [
-  {path: 'hero', component: HeroComponent},
-  {path: 'magic', component: MagicComponent},
-  {
-    path: "",
-    redirectTo: 'hero',
-    pathMatch: "full"
-  },
-];
-*/
+
 @NgModule({
-  imports: [
-    BrowserModule,
-    // RouterModule.forRoot(appRoutes, {useHash: true}), // TODO move in proper module
-    BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
-  ],
-  declarations: [AppComponent, /*HeroComponent, MagicComponent*/],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		RoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		FlexLayoutModule
+	],
+	declarations: [AppComponent, AthleteSettingsComponent, CommonSettingsComponent],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
---
+++ plugin/webapp/src/app/common-settings/common-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-settings',
+  templateUrl: './common-settings.component.html',
+  styleUrls: ['./common-settings.component.css']
+})
+export class CommonSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
