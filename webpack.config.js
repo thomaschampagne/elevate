846bd05d3985f1961747dbfe8f0009d48291959a
Grzegorz Jazurek <grzegorz.jazurek@gmail.com>
2017-08-30 12:30:24 +0200

    create simple archive task (without preview option)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const fs = require("fs");
+const moment = require("moment");
 const { SourceMapDevToolPlugin } = require("webpack");
-const webappConfig = require('./config/webapp.webpack.config');
-const coreConfig = require('./config/core.webpack.config');
+const webappConfig = require("./config/webapp.webpack.config");
+const coreConfig = require("./config/core.webpack.config");
+const WebpackZipPlugin = require("webpack-zip-plugin");
 
 module.exports = function (env) {
   if (env && env.dev) {
@@ -16,6 +19,20 @@ module.exports = function (env) {
         "sourceRoot": "webpack:///"
       })
     );
+
+    if (env && env.archive) {
+      const version = JSON.parse(fs.readFileSync("./plugin/manifest.json")).version;
+      const currentDateString = moment().format("YYYY-MM-DD_hh.mm.ss");
+
+      const archiveName = `stravistix_${version}_${currentDateString}.zip`;
+      coreConfig.plugins.push(
+        new WebpackZipPlugin({
+          initialFile: "./dist",
+          endPath: "./package",
+          zipName: archiveName
+        })
+      );
+    }
   }
 
   return [webappConfig, coreConfig];
