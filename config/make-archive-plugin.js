78ac9e6c2c7cfb71f4d0d4a8b2c8721afd8e49dd
Grzegorz Jazurek <grzegorz.jazurek@gmail.com>
2017-08-31 10:32:26 +0200

    create make archive plugin

---
+++ config/make-archive-plugin.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const exec = require("child_process").exec;
+const zipFolder = require("zip-folder");
+const moment = require("moment");
+
+module.exports = MakeArchivePlugin;
+
+function MakeArchivePlugin(options) {
+  if (!options || !options.sourceDir || !options.destinationDir || !options.manifestPath) {
+    throw Error("options should be object which contains sourceDir destinationDir and manifestPath properties");
+  }
+  this.options = options;
+}
+
+MakeArchivePlugin.prototype.apply = function (compiler) {
+  const options = this.options;
+
+  compiler.plugin("done", (stats) => {
+
+    if (!fs.existsSync(options.destinationDir)) {
+      fs.mkdirSync(options.destinationDir);
+    }
+
+    if (options.preview) {
+      getGitShortSha1((sha1) => {
+        const versionName = sha1;
+        modifyManifest(options.manifestPath, sha1, () => {
+          saveZipFile(options.sourceDir, options.destinationDir, versionName);
+        });
+      });
+    } else {
+      const versionName = getVersionFromManifest(options.manifestPath);
+      saveZipFile(options.sourceDir, options.destinationDir, versionName);
+    }
+  });
+}
+
+/**
+ * get short version of HEAD sha1 hash
+ * @param {function} callback (shortSha1) => {...}
+ */
+function getGitShortSha1(callback) {
+  exec("git rev-parse --short HEAD", (error, stdout) => {
+    if (error) {
+      throw error;
+    }
+    callback(stdout.trim());
+  });
+}
+
+/**
+ * get current plugin version from manifest
+ * @param {string} manifestPath path to manifest.json file
+ */
+function getVersionFromManifest(manifestPath) {
+  return `v${JSON.parse(fs.readFileSync(manifestPath)).version}`;
+}
+
+/**
+ * modify manifest version
+ * @param {string} manifestPath manifest.json path
+ * @param {string} shortSha1 short sha1 hash
+ * @param {function} callback notify about modify manifest file finish
+ */
+function modifyManifest(manifestPath, shortSha1, callback) {
+  fs.readFile(manifestPath, "utf8", (err, fileContent) => {
+    if (err) {
+      throw err;
+    }
+
+    const versionName = `preview@${shortSha1}`;
+
+    const manifestJson = JSON.parse(fileContent);
+
+    manifestJson.version = "0";
+    manifestJson.version_name = versionName;
+
+    const updatedManifest = JSON.stringify(manifestJson, null, 2)
+
+    fs.writeFile(manifestPath, updatedManifest, callback);
+  });
+}
+
+/**
+ * Create zip archive base on source directory
+ * @param {string} sourceDir source directory
+ * @param {string} destinationDir destination directory
+ * @param {string} versionName version name
+ */
+function saveZipFile(sourceDir, destinationDir, versionName) {
+  const currentDateString = moment().format("YYYY-MM-DD_hh.mm.ss");
+  const archiveName = `stravistix_${versionName}_${currentDateString}.zip`;
+
+  zipFolder(sourceDir, `${destinationDir}/${archiveName}`, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-const fs = require("fs");
-const moment = require("moment");
 const { SourceMapDevToolPlugin } = require("webpack");
 const webappConfig = require("./config/webapp.webpack.config");
 const coreConfig = require("./config/core.webpack.config");
-const WebpackZipPlugin = require("webpack-zip-plugin");
+const MakeArchivePlugin = require("./config/make-archive-plugin");
 
 module.exports = function (env) {
   if (env && env.dev) {
@@ -21,15 +19,13 @@ module.exports = function (env) {
     );
 
     if (env && env.archive) {
-      const version = JSON.parse(fs.readFileSync("./plugin/manifest.json")).version;
-      const currentDateString = moment().format("YYYY-MM-DD_hh.mm.ss");
 
-      const archiveName = `stravistix_${version}_${currentDateString}.zip`;
       coreConfig.plugins.push(
-        new WebpackZipPlugin({
-          initialFile: "./dist",
-          endPath: "./package",
-          zipName: archiveName
+        new MakeArchivePlugin({
+          sourceDir: "./dist",
+          destinationDir: "./package",
+          manifestPath: "./dist/manifest.json",
+          preview: !!env.preview
         })
       );
     }
