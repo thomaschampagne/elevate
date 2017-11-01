4aefa66c1bb11203a236eab5ecaf941a09048d0b
Thomas Champagne <th.champagne@gmail.com>
2017-11-01 16:50:14 +0100

    fix #471

--- plugin/core/scripts/modifiers/HideFeedModifier.ts
+++ plugin/core/scripts/modifiers/HideFeedModifier.ts
@@ -22,7 +22,6 @@ export class HideFeedModifier implements IModifier {
             // If hide challenges
             if (this.userSettings.feedHideChallenges) {
                 $(".feed-container").find(".challenge").remove();
-
             }
 
             // If hide created routes
@@ -34,7 +33,7 @@ export class HideFeedModifier implements IModifier {
 
             // If hide suggested athletes
             if (this.userSettings.feedHideSuggestedAthletes) {
-                $('#suggested-follow-module').remove(); // Will work as long as id remains "suggested-follow-module"
+                $("#suggested-follow-module").remove(); // Will work as long as id remains "suggested-follow-module"
             }
 
             if (this.userSettings.feedHideVirtualRides || this.userSettings.feedHideRideActivitiesUnderDistance > 0 || this.userSettings.feedHideRunActivitiesUnderDistance > 0) {
@@ -44,30 +43,36 @@ export class HideFeedModifier implements IModifier {
 
                 $("div.feed>.activity").each((index: number, element: Element) => {
 
-                    const type: string = $(element).find(".entry-type-icon .app-icon").attr("class").replace("icon-lg", "").replace("app-icon", "").replace("icon-dark", "").replace(/\s+/g, "").replace("icon-", "");
+                    const activityType: string = $(element)
+                        .find("div.entry-icon.media-left").find(".app-icon").attr("class")
+                        .replace("app-icon", "")
+                        .replace("icon-dark", "")
+                        .replace("icon-lg", "")
+                        .replace(/\s+/g, "")
+                        .replace("icon-", "");
 
-                    const distanceEl = _.filter($(element).find("ul.inline-stats").find("[class=unit]"), function(item) {
+                    const distanceElement = _.filter($(element).find("ul.list-stats").find("[class=unit]"), (item) => {
                         return ($(item).html() == "km" || $(item).html() == "mi");
                     });
 
-                    const distance: number = parseFloat($(distanceEl).parent().text().replace(",", "."));
+                    const distance: number = parseFloat($(distanceElement).parent().text().replace(",", "."));
 
                     // Remove virtual rides
-                    if (this.userSettings.feedHideVirtualRides && type === HideFeedModifier.VIRTUAL_RIDE) {
+                    if (this.userSettings.feedHideVirtualRides && activityType === HideFeedModifier.VIRTUAL_RIDE) {
                         $(element).remove();
                     }
 
                     // Remove Ride activities if distance lower than "minRideDistanceToHide", if minRideDistanceToHide equal 0, then keep all.
-                    if ((minRideDistanceToHide > 0) && distance && (distance < minRideDistanceToHide) && (type === HideFeedModifier.RIDE || type === HideFeedModifier.VIRTUAL_RIDE)) {
+                    if ((minRideDistanceToHide > 0) && distance && (distance < minRideDistanceToHide) && (activityType === HideFeedModifier.RIDE || activityType === HideFeedModifier.VIRTUAL_RIDE)) {
                         $(element).remove();
                     }
 
                     // Remove Run activities if distance lower than "minRunDistanceToHide", if minRunDistanceToHide equal 0, then keep all.
-                    if ((minRunDistanceToHide > 0) && distance && (distance < minRunDistanceToHide) && type === HideFeedModifier.RUN) {
+                    if ((minRunDistanceToHide > 0) && distance && (distance < minRunDistanceToHide) && activityType === HideFeedModifier.RUN) {
+                        debugger;
                         $(element).remove();
                     }
                 });
-
             }
 
             // Cleaning time container with no activites
