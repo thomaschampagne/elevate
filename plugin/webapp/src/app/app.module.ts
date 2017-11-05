caa65fde902d40e87a1ce681123d31ab80d59f2d
Thomas Champagne <th.champagne@gmail.com>
2017-11-05 13:49:40 +0100

    Remove old webapp (ng4) by new one (ng5)

--- plugin/webapp/src/app/app.module.ts
+++ plugin/webapp/src/app/app.module.ts
@@ -1,27 +1,16 @@
-import "hammerjs";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./shared/material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
-
-import { AppComponent } from "./app.component";
-import { AthleteSettingsComponent } from './athlete-settings/athlete-settings.component';
-import { CommonSettingsComponent } from './common-settings/common-settings.component';
-import { RoutingModule } from "./shared/routing.module";
-
+import { AppComponent } from './app.component';
 
 @NgModule({
-	imports: [
-		BrowserModule,
-		RoutingModule,
-		BrowserAnimationsModule,
-		MaterialModule,
-		FlexLayoutModule
-	],
-	declarations: [AppComponent, AthleteSettingsComponent, CommonSettingsComponent],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
