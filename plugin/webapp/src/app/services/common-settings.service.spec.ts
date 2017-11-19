4c615570de08cddbc38c442119b2dab97b265028
Thomas Champagne <th.champagne@gmail.com>
2017-11-20 00:23:45 +0100

    Added option helpers test

--- plugin/webapp/src/app/services/common-settings.service.spec.ts
+++ plugin/webapp/src/app/services/common-settings.service.spec.ts
@@ -2,14 +2,14 @@ import { inject, TestBed } from '@angular/core/testing';
 
 import { CommonSettingsService } from './common-settings.service';
 
-xdescribe('CommonSettingsService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CommonSettingsService]
-    });
-  });
+describe('CommonSettingsService', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [CommonSettingsService]
+		});
+	});
 
-  it('should be created', inject([CommonSettingsService], (service: CommonSettingsService) => {
-    expect(service).toBeTruthy();
-  }));
+	it('should be created', inject([CommonSettingsService], (service: CommonSettingsService) => {
+		expect(service).toBeTruthy();
+	}));
 });
--- plugin/webapp/src/app/services/option-helper-reader.service.spec.ts
+++ plugin/webapp/src/app/services/option-helper-reader.service.spec.ts
@@ -1,15 +1,48 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { OptionHelperReaderService } from './option-helper-reader.service';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
 
-xdescribe('OptionHelperReaderService', () => {
+describe('OptionHelperReaderService', () => {
+
+	let http: HttpClient;
+	let optionHelperReaderService: OptionHelperReaderService;
+
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			providers: [OptionHelperReaderService]
+			imports: [HttpClientModule],
+			providers: [OptionHelperReaderService, HttpClient]
 		});
+
+		http = TestBed.get(HttpClient);
+		optionHelperReaderService = TestBed.get(OptionHelperReaderService);
 	});
 
-	it('should be created', inject([OptionHelperReaderService], (service: OptionHelperReaderService) => {
-		expect(service).toBeTruthy();
-	}));
+	it('should be created', () => {
+		expect(optionHelperReaderService).not.toBeNull();
+		expect(http).not.toBeNull();
+
+	});
+
+	it('should return markdown data', (done: Function) => {
+
+		// Given
+		const markDownData = "## **This Title has bold style**";
+
+		spyOn(http, 'get').and.returnValue(Observable.of(markDownData));
+
+		// When
+		const promise = optionHelperReaderService.get('test.md');
+
+		// Then
+		promise.then((markDownResultData: string) => {
+			expect(markDownResultData).not.toBeNull();
+			expect(markDownResultData).toBe(markDownData);
+			done();
+		});
+
+	});
 });
+
