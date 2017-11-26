6a542cfeff568cfcda6ed08f0bc4031c60adfbac
Thomas Champagne <th.champagne@gmail.com>
2017-11-26 22:00:00 +0000

    Metrics graphics first draw !

--- plugin/webapp/src/typings.d.ts
+++ plugin/webapp/src/typings.d.ts
@@ -1,5 +1,13 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+
 interface NodeModule {
-  id: string;
+	id: string;
 }
+
+/**
+ * Metrics Graphics global var declaration
+ * No types exists currently on that library
+ * source: https://github.com/mozilla/metrics-graphics
+ */
+declare const MG: any;
