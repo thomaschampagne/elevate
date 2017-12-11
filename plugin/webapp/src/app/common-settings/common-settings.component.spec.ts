ca830a385575dde4f18b58581020639c7b05a17b
Thomas Champagne <th.champagne@gmail.com>
2017-12-12 00:13:31 +0100

    Add some missing unit testing component base

--- plugin/webapp/src/app/common-settings/common-settings.component.spec.ts
+++ plugin/webapp/src/app/common-settings/common-settings.component.spec.ts
@@ -1,16 +1,36 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CommonSettingsComponent } from "./common-settings.component";
+import { UserSettingsDao } from "../shared/dao/user-settings/user-settings.dao";
+import { SharedModule } from "../shared/shared.module";
+import * as _ from "lodash";
+import { userSettings } from "../../../../common/scripts/UserSettings";
+import { CoreModule } from "../core/core.module";
 
-xdescribe("CommonSettingsComponent", () => {
+describe("CommonSettingsComponent", () => {
+
 	let component: CommonSettingsComponent;
 	let fixture: ComponentFixture<CommonSettingsComponent>;
+	let userSettingsDao: UserSettingsDao;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [CommonSettingsComponent]
-		})
-			.compileComponents();
+			imports: [
+				CoreModule,
+				SharedModule,
+			],
+			declarations: [],
+			providers: []
+		}).compileComponents();
+
+		userSettingsDao = TestBed.get(UserSettingsDao);
+
+		spyOn(userSettingsDao, "chromeStorageSync").and.returnValue({
+			get: (keys: any, callback: (item: Object) => {}) => {
+				callback(_.cloneDeep(userSettings));
+			}
+		});
+
 	}));
 
 	beforeEach(() => {
@@ -21,5 +41,32 @@ xdescribe("CommonSettingsComponent", () => {
 
 	it("should create", () => {
 		expect(component).toBeTruthy();
+	});
+
+	it("should get option helper dir", () => {
+
+		// Given
+		const pathname = "/webapp/dist/index.html";
+		const expected = "/webapp/dist/assets/option-helpers/";
+
+		// When
+		const actual = CommonSettingsComponent.getOptionHelperDir(pathname);
+
+		// Then
+		expect(actual).toEqual(expected);
+
+	});
+
+	it("should get option helper dir", () => {
+
+		// Given
+		const pathname = null;
+
+		// When
+		const actual = CommonSettingsComponent.getOptionHelperDir(pathname);
+
+		// Then
+		expect(actual).toBeNull();
+
 	});
 });
