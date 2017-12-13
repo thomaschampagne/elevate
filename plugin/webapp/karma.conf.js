3852695fdac27763315441a19f1e5f92f4b17d9f
Thomas Champagne <th.champagne@gmail.com>
2017-12-13 18:40:20 +0100

    Add chrome headless karma testing config

--- plugin/webapp/karma.conf.js
+++ plugin/webapp/karma.conf.js
@@ -27,7 +27,23 @@ module.exports = function (config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome'],
+		browsers: [
+			'Chrome',
+			// 'ChromeNoSandboxHeadless'
+		],
+		customLaunchers: {
+			ChromeNoSandboxHeadless: {
+				base: 'Chrome',
+				flags: [
+					'--no-sandbox',
+					// See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+					'--headless',
+					'--disable-gpu',
+					// Without a remote debugging port, Google Chrome exits immediately.
+					' --remote-debugging-port=9222',
+				],
+			},
+		},
 		singleRun: false
 	});
 };
