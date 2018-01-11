f65d7a7edadb0c443ca4b8c6639803e1c7cc9028
Thomas Champagne <th.champagne@gmail.com>
2018-01-12 00:15:26 +0100

    Year progress helper dialog content

--- plugin/webapp/src/app/year-progress/year-progress-helper-dialog/year-progress-helper-dialog.component.ts
+++ plugin/webapp/src/app/year-progress/year-progress-helper-dialog/year-progress-helper-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from "moment";
+import { Moment } from "moment";
 
 @Component({
 	selector: 'app-year-progress-helper-dialog',
@@ -10,10 +12,13 @@ export class YearProgressHelperDialogComponent implements OnInit {
 	public static readonly MAX_WIDTH: string = "80%";
 	public static readonly MIN_WIDTH: string = "40%";
 
+	public todayMoment: Moment;
+
 	constructor() {
 	}
 
 	public ngOnInit(): void {
+		this.todayMoment = moment();
 	}
 
 }
