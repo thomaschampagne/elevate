92acad972301d68ec14f110c0f426118e1f26711
Thomas Champagne <th.champagne@gmail.com>
2018-01-16 00:27:21 +0100

    AthleteHistoryImportDialogComponent spec

--- plugin/webapp/src/app/shared/dialogs/athlete-history-import-dialog/athlete-history-import-dialog.component.spec.ts
+++ plugin/webapp/src/app/shared/dialogs/athlete-history-import-dialog/athlete-history-import-dialog.component.spec.ts
@@ -1,15 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AthleteHistoryImportDialogComponent } from './athlete-history-import-dialog.component';
+import { CoreModule } from "../../../core/core.module";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { SharedModule } from "../../shared.module";
 
-xdescribe('AthleteHistoryImportDialogComponent', () => {
+describe('AthleteHistoryImportDialogComponent', () => {
 
 	let component: AthleteHistoryImportDialogComponent;
 	let fixture: ComponentFixture<AthleteHistoryImportDialogComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [AthleteHistoryImportDialogComponent]
+			imports: [
+				CoreModule,
+				SharedModule,
+			],
+			declarations: [],
+			providers: [
+				{
+					provide: MAT_DIALOG_DATA, useValue: {},
+				},
+				{
+					provide: MatDialogRef, useValue: {},
+				},
+			]
 		}).compileComponents();
 	}));
 
