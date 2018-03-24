110c3d0b99e1a97c767a09acdec3cccbdec454b5
Thomas Champagne <th.champagne@gmail.com>
2018-03-24 14:05:54 +0100

    #341 Fitness trend settings dialog warn when TRIMP

--- plugin/app/src/app/fitness-trend/fitness-trend-inputs/fitness-trend-settings-dialog/fitness-trend-settings-dialog.component.ts
+++ plugin/app/src/app/fitness-trend/fitness-trend-inputs/fitness-trend-settings-dialog/fitness-trend-settings-dialog.component.ts
@@ -1,12 +1,12 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { FitnessTrendSettingsModel } from "../models/fitness-trend-settings.model";
 import { HeartRateImpulseMode } from "../../shared/enums/heart-rate-impulse-mode.enum";
 
 @Component({
-	selector: 'app-fitness-trend-settings-dialog',
-	templateUrl: './fitness-trend-settings-dialog.component.html',
-	styleUrls: ['./fitness-trend-settings-dialog.component.scss']
+	selector: "app-fitness-trend-settings-dialog",
+	templateUrl: "./fitness-trend-settings-dialog.component.html",
+	styleUrls: ["./fitness-trend-settings-dialog.component.scss"]
 })
 export class FitnessTrendSettingsDialogComponent implements OnInit {
 
@@ -19,14 +19,18 @@ export class FitnessTrendSettingsDialogComponent implements OnInit {
 				@Inject(MAT_DIALOG_DATA) public settingsModel: FitnessTrendSettingsModel) {
 	}
 
-	public ngOnInit() {
+	public ngOnInit(): void {
 	}
 
-	public onSave() {
+	public onSave(): void {
 		this.dialogRef.close(this.settingsModel);
 	}
 
-	public onCancel() {
+	public onCancel(): void {
 		this.dialogRef.close();
+	}
+
+	public onModeChange(): void {
+		this.settingsModel.heartRateImpulseMode = Number(this.settingsModel.heartRateImpulseMode);
 	}
 }
