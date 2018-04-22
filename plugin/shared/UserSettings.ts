562b8a4763a2c47cf9337ed33cc16169b6ae3413
Thomas Champagne <th.champagne@gmail.com>
2018-04-22 14:05:18 +0200

    Reduce full auto-sync period to 6 hours

--- plugin/shared/UserSettings.ts
+++ plugin/shared/UserSettings.ts
@@ -1,7 +1,7 @@
 import { UserSettingsModel } from "./models/user-settings/user-settings.model";
 
 export const userSettings: UserSettingsModel = {
-	autoSyncMinutes: 720,
+	autoSyncMinutes: 360,
 	localStorageMustBeCleared: false,
 	systemUnit: UserSettingsModel.SYSTEM_UNIT_METRIC_KEY,
 	userGender: "men",
