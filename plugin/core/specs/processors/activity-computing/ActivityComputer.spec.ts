f27370573231a45fa268737e16b011547f5fbc99
Thomas Champagne <th.champagne@gmail.com>
2018-05-26 15:14:50 +0200

    Fix CI tests

--- plugin/core/specs/processors/activity-computing/ActivityComputer.spec.ts
+++ plugin/core/specs/processors/activity-computing/ActivityComputer.spec.ts
@@ -50,15 +50,15 @@ describe("ActivityComputer", () => {
 		expect(result.paceData.variancePace.toString()).toMatch(/^47.995052362/);
 
 		expect(result.powerData.hasPowerMeter).toEqual(false);
-		expect(result.powerData.avgWatts.toString()).toMatch(/^210.685980088/);
-		expect(result.powerData.avgWattsPerKg.toString()).toMatch(/^2.9302639789/);
-		expect(result.powerData.weightedPower.toString()).toMatch(/^245.18579465/);
-		expect(result.powerData.variabilityIndex.toString()).toMatch(/^1.16374993034/);
-		expect(result.powerData.punchFactor.toString()).toMatch(/^1.021607477/);
-		expect(result.powerData.weightedWattsPerKg.toString()).toMatch(/^3.4100945014/);
-		expect(result.powerData.lowerQuartileWatts.toString()).toMatch(/^92/);
-		expect(result.powerData.medianWatts.toString()).toMatch(/^204/);
-		expect(result.powerData.upperQuartileWatts.toString()).toMatch(/^304/);
+		expect(result.powerData.avgWatts.toString()).toMatch(/^208.14/);
+		expect(result.powerData.avgWattsPerKg.toString()).toMatch(/^2.89/);
+		expect(result.powerData.weightedPower.toString()).toMatch(/^222.39/);
+		expect(result.powerData.variabilityIndex.toString()).toMatch(/^1.06/);
+		expect(result.powerData.punchFactor.toString()).toMatch(/^0.92/);
+		expect(result.powerData.weightedWattsPerKg.toString()).toMatch(/^3.09/);
+		expect(result.powerData.lowerQuartileWatts.toString()).toMatch(/^187/);
+		expect(result.powerData.medianWatts.toString()).toMatch(/^215/);
+		expect(result.powerData.upperQuartileWatts.toString()).toMatch(/^238/);
 
 		expect(result.heartRateData.TRIMP.toString()).toMatch(/^228.48086657/);
 		expect(result.heartRateData.TRIMPPerHour.toString()).toMatch(/^134.2688736/);
