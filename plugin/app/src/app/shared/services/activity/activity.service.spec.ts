74aa606690beda9f8f2c10c1c9bb78af1a6badc0
Thomas Champagne <th.champagne@gmail.com>
2018-06-05 00:52:26 +0200

    Add ActivityService::removeByIds

--- plugin/app/src/app/shared/services/activity/activity.service.spec.ts
+++ plugin/app/src/app/shared/services/activity/activity.service.spec.ts
@@ -107,4 +107,45 @@ describe("ActivityService", () => {
 			done();
 		});
 	});
+
+	it("should remove SyncedActivityModel by strava activity ids", (done: Function) => {
+
+		// Given
+		const activitiesToDelete = [
+			302537043, // Chamrousse 1750
+			296692980, // Fondo 100
+		];
+
+		const expectedExistingActivity = 353633586; // Venon PR 01
+
+		spyOn(activityService.activityDao, "removeByIds")
+			.and.returnValue(Promise.resolve(_.filter(_TEST_SYNCED_ACTIVITIES_, (syncedActivityModel: SyncedActivityModel) => {
+			return (_.indexOf(activitiesToDelete, syncedActivityModel.id) === -1);
+		})));
+
+		// When
+		const promise: Promise<SyncedActivityModel[]> = activityService.removeByIds(activitiesToDelete);
+
+		// Then
+		promise.then((result: SyncedActivityModel[]) => {
+
+			expect(result.length).toEqual(_TEST_SYNCED_ACTIVITIES_.length - activitiesToDelete.length);
+
+			let activity = _.find(result, {id: activitiesToDelete[0]});
+			expect(_.isEmpty(activity)).toBeTruthy();
+
+			activity = _.find(result, {id: activitiesToDelete[1]});
+			expect(_.isEmpty(activity)).toBeTruthy();
+
+			activity = _.find(result, {id: expectedExistingActivity});
+			expect(_.isEmpty(activity)).toBeFalsy();
+
+			done();
+
+		}, error => {
+			expect(error).toBeNull();
+			expect(false).toBeTruthy("Whoops! I should not be here!");
+			done();
+		});
+	});
 });
