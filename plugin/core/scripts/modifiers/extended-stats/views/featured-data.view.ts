e1a716f164c27eae04119b81c57ef31e01a87953
Thomas Champagne <th.champagne@gmail.com>
2018-10-13 14:52:23 +0200

    close #654

--- plugin/core/scripts/modifiers/extended-stats/views/featured-data.view.ts
+++ plugin/core/scripts/modifiers/extended-stats/views/featured-data.view.ts
@@ -60,8 +60,8 @@ export class FeaturedDataView extends AbstractDataView {
 		}
 
 		if (this.analysisData.heartRateData && this.userSettings.displayAdvancedHrData) {
-			this.insertContentAtGridPosition(2, 0, this.printNumber(this.analysisData.heartRateData.HRSS, 0), "HRSS <sup style='color:#FC4C02; font-size:12px; position: initial;'>NEW</sup>", "", "displayAdvancedHrData");
-			this.insertContentAtGridPosition(3, 0, this.printNumber(this.analysisData.heartRateData.HRSSPerHour, 1), "HRSS / Hour <sup style='color:#FC4C02; font-size:12px; position: initial;'>NEW</sup>", "", "displayAdvancedHrData");
+			this.insertContentAtGridPosition(2, 0, this.printNumber(this.analysisData.heartRateData.HRSS, 0), "HRSS", "", "displayAdvancedHrData");
+			this.insertContentAtGridPosition(3, 0, this.printNumber(this.analysisData.heartRateData.HRSSPerHour, 1), "HRSS / Hour", "", "displayAdvancedHrData");
 		}
 
 		if (this.analysisData.powerData && this.userSettings.displayAdvancedPowerData) {
@@ -71,7 +71,7 @@ export class FeaturedDataView extends AbstractDataView {
 				if (!this.analysisData.powerData.hasPowerMeter) {
 					label = "Estimated " + label;
 				}
-				this.insertContentAtGridPosition(4, 0, this.printNumber(this.analysisData.powerData.best20min, 0), label + " <sup style='color:#FC4C02; font-size:12px; position: initial;'>NEW</sup>", "w", "displayAdvancedPowerData"); // Avg watt /kg
+				this.insertContentAtGridPosition(4, 0, this.printNumber(this.analysisData.powerData.best20min, 0), label, "w", "displayAdvancedPowerData"); // Avg watt /kg
 			}
 
 			if (_.isNumber(this.analysisData.powerData.weightedWattsPerKg)) {
