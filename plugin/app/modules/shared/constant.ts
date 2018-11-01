da8b9460a00bafa360432ee3aa7cfaaa71360c16
Thomas Champagne <th.champagne@gmail.com>
2018-11-01 20:58:04 +0100

    Use https instead of http in constant.ts

--- plugin/app/modules/shared/constant.ts
+++ plugin/app/modules/shared/constant.ts
@@ -1,6 +1,6 @@
 export class Constant {
 
-	public static readonly LANDING_PAGE_URL: string = "http://thomaschampagne.github.io/elevate/";
+	public static readonly LANDING_PAGE_URL: string = "https://thomaschampagne.github.io/elevate/";
 	public static readonly APP_ROOT_URL: string = "/app/index.html";
 
 
