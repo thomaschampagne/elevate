a50686bfe9a909cc74bb2ec89b1a9ffc7c2ebb85
Thomas Champagne <th.champagne@gmail.com>
2018-11-10 14:59:20 +0100

    #688 Feedbacks

--- plugin/app/src/app/shared/data-store/impl/spec/mocked-data-store.service.ts
+++ plugin/app/src/app/shared/data-store/impl/spec/mocked-data-store.service.ts
@@ -1,5 +1,6 @@
 import { DataStore } from "../../data-store";
 import { StorageLocationModel } from "../../storage-location.model";
+import * as _ from "lodash";
 
 export class MockedDataStore<T> extends DataStore<T> {
 
@@ -11,7 +12,7 @@ export class MockedDataStore<T> extends DataStore<T> {
 		if (initObject) {
 			this.dataStore = initObject;
 		} else {
-			this.setTypeVector();
+			this.initWithVector();
 		}
 	}
 
@@ -37,15 +38,20 @@ export class MockedDataStore<T> extends DataStore<T> {
 
 		const isNestedPath: boolean = (path instanceof Array && path.length > 0);
 		const rootKey: string = (isNestedPath) ? path[0] : path as string;
-		const hasRootKey = (rootKey && this.dataStore[rootKey]);
+		const hasRootKey = (rootKey && _.has(this.dataStore, rootKey));
 
 		if (!hasRootKey) {
-			throw new Error("No root key found for:" + this.dataStore);
+			return Promise.reject("No root key '" + rootKey + "' found");
 		}
 
 		// Update store
 		if (isNestedPath) {
-			this.dataStore = DataStore.setAtPath(this.dataStore, path as string[], value);
+			try {
+				this.dataStore = DataStore.setAtPath(this.dataStore, path as string[], value);
+			} catch (error) {
+				return Promise.reject(error.message);
+			}
+
 		} else {
 			this.dataStore[rootKey] = value;
 		}
@@ -53,11 +59,11 @@ export class MockedDataStore<T> extends DataStore<T> {
 		return Promise.resolve(<T> this.dataStore);
 	}
 
-	public setTypeVector() {
-		this.dataStore = [];
+	public initWithVector(vector?: T[]) {
+		this.dataStore = (vector && vector.length > 0) ? vector : [];
 	}
 
-	public setTypeObject() {
-		this.dataStore = {} as T;
+	public initWithObject(object?: T) {
+		this.dataStore = (object) ? object : {} as T;
 	}
 }
