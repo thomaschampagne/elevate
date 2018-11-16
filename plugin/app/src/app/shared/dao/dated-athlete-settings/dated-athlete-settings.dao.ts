446eb5884e67932564e2f7cd763a0801698e14ff
Thomas Champagne <th.champagne@gmail.com>
2018-11-16 21:54:15 +0100

    Migrating DatedAthleteSettingsDao to new BaseDao/DataStore implementation

--- plugin/app/src/app/shared/dao/dated-athlete-settings/dated-athlete-settings.dao.ts
+++ plugin/app/src/app/shared/dao/dated-athlete-settings/dated-athlete-settings.dao.ts
@@ -1,72 +1,20 @@
 import { Injectable } from "@angular/core";
-import * as _ from "lodash";
-import { DatedAthleteSettingsModel } from "@elevate/shared/models";
+import { AppStorageType, DatedAthleteSettingsModel } from "@elevate/shared/models";
+import { BaseDao } from "../base.dao";
+import { StorageLocationModel } from "../../data-store/storage-location.model";
 
 @Injectable()
-export class DatedAthleteSettingsDao {
+export class DatedAthleteSettingsDao extends BaseDao<DatedAthleteSettingsModel> {
 
-	public static readonly DATED_ATHLETE_SETTINGS_KEY: string = "datedAthleteSettings";
+	public static readonly STORAGE_LOCATION: StorageLocationModel = new StorageLocationModel(AppStorageType.LOCAL, "datedAthleteSettings");
+	public static readonly DEFAULT_STORAGE_VALUE: DatedAthleteSettingsModel[] = [];
 
-	constructor() {
+	public getDefaultStorageValue(): DatedAthleteSettingsModel[] | DatedAthleteSettingsModel {
+		return DatedAthleteSettingsDao.DEFAULT_STORAGE_VALUE;
 	}
 
-	public fetch(): Promise<DatedAthleteSettingsModel[]> {
-
-		return new Promise<DatedAthleteSettingsModel[]>((resolve: Function, reject: Function) => {
-
-			this.browserStorageLocal().get(DatedAthleteSettingsDao.DATED_ATHLETE_SETTINGS_KEY, (result: { datedAthleteSettings: DatedAthleteSettingsModel[] }) => {
-				const error = this.getChromeError();
-				if (error) {
-					reject(error.message);
-				} else {
-					const datedAthleteSettingsModels = (_.isEmpty(result.datedAthleteSettings)) ? [] : result.datedAthleteSettings;
-					resolve(datedAthleteSettingsModels);
-				}
-			});
-
-		});
-	}
-
-	public save(datedAthleteSettingsModels: DatedAthleteSettingsModel[]): Promise<DatedAthleteSettingsModel[]> {
-
-		return new Promise<DatedAthleteSettingsModel[]>((resolve: Function, reject: Function) => {
-
-			datedAthleteSettingsModels = _.sortBy(datedAthleteSettingsModels, (model: DatedAthleteSettingsModel) => {
-				const sortOnDate: Date = (_.isNull(model.since)) ? new Date(0) : new Date(model.since);
-				return sortOnDate.getTime() * -1;
-			});
-
-			const datedAthleteSettingsData = {};
-			datedAthleteSettingsData[DatedAthleteSettingsDao.DATED_ATHLETE_SETTINGS_KEY] = datedAthleteSettingsModels;
-
-			this.browserStorageLocal().set(datedAthleteSettingsData, () => {
-				const error = this.getChromeError();
-				if (error) {
-					reject(error.message);
-				} else {
-					this.fetch().then((models: DatedAthleteSettingsModel[]) => {
-						resolve(models);
-					});
-				}
-			});
-
-		});
-	}
-
-	/**
-	 *
-	 * @returns {chrome.storage.SyncStorageArea}
-	 */
-	public browserStorageLocal(): chrome.storage.LocalStorageArea {
-		return chrome.storage.local;
-	}
-
-	/**
-	 *
-	 * @returns {chrome.runtime.LastError}
-	 */
-	public getChromeError(): chrome.runtime.LastError {
-		return chrome.runtime.lastError;
+	public getStorageLocation(): StorageLocationModel {
+		return DatedAthleteSettingsDao.STORAGE_LOCATION;
 	}
 
 }
