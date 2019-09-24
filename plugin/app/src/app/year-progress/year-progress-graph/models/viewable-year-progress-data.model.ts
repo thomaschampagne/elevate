31763fded967d78dffe13b4ed41f05823a6e6ce2
Thomas Champagne <th.champagne@gmail.com>
2019-09-25 01:22:09 +0200

    Apply projects changes required by Ivy

--- plugin/app/src/app/year-progress/year-progress-graph/models/viewable-year-progress-data.model.ts
+++ plugin/app/src/app/year-progress/year-progress-graph/models/viewable-year-progress-data.model.ts
@@ -1,6 +1,7 @@
 import { GraphPointModel } from "../../../shared/models/graphs/graph-point.model";
 import { MarkerModel } from "../../../fitness-trend/fitness-trend-graph/models/marker.model";
 import * as _ from "lodash";
+import * as MG from "metrics-graphics";
 import { Moment } from "moment";
 
 export class ViewableYearProgressDataModel {
