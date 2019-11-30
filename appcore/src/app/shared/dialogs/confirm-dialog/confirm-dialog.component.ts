74b59526d613c7ec4bb07962ec237a96608f39fc
Thomas Champagne <th.champagne@gmail.com>
2019-11-30 15:50:35 +0100

    Support html in confirm dialog

--- appcore/src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ appcore/src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { ConfirmDialogDataModel } from "./confirm-dialog-data.model";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
 	selector: "app-confirm-dialog",
@@ -12,11 +13,15 @@ export class ConfirmDialogComponent implements OnInit {
 	public static readonly MAX_WIDTH: string = "80%";
 	public static readonly MIN_WIDTH: string = "40%";
 
+	public html: string;
+
 	constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+				public domSanitizer: DomSanitizer,
 				@Inject(MAT_DIALOG_DATA) public dialogData: ConfirmDialogDataModel) {
 	}
 
 	public ngOnInit() {
+		this.html = <string> this.domSanitizer.bypassSecurityTrustHtml(this.dialogData.content);
 	}
 
 	public onConfirm() {
