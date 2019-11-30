9fdb1cd8b5b037c0491552469c915b5c641a24b3
Thomas Champagne <th.champagne@gmail.com>
2019-11-30 15:51:03 +0100

    Support html in got it dialog

--- appcore/src/app/shared/dialogs/got-it-dialog/got-it-dialog.component.ts
+++ appcore/src/app/shared/dialogs/got-it-dialog/got-it-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { GotItDialogDataModel } from "./got-it-dialog-data.model";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
 	selector: "app-got-it-dialog",
@@ -12,9 +13,13 @@ export class GotItDialogComponent implements OnInit {
 	public static readonly MAX_WIDTH: string = "80%";
 	public static readonly MIN_WIDTH: string = "40%";
 
-	constructor(@Inject(MAT_DIALOG_DATA) public dialogData: GotItDialogDataModel) {
+	public html: string;
+
+	constructor(@Inject(MAT_DIALOG_DATA) public dialogData: GotItDialogDataModel,
+				public domSanitizer: DomSanitizer) {
 	}
 
 	public ngOnInit(): void {
+		this.html = <string> this.domSanitizer.bypassSecurityTrustHtml(this.dialogData.content);
 	}
 }
