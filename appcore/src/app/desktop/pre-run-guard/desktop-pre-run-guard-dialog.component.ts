41fce13353bd74e8cf6b10a45408e347bbf46bad
Thomas Champagne <th.champagne@gmail.com>
2020-02-21 19:04:56 +0100

    Change message displayed for non allowed computer

--- appcore/src/app/desktop/pre-run-guard/desktop-pre-run-guard-dialog.component.ts
+++ appcore/src/app/desktop/pre-run-guard/desktop-pre-run-guard-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { ElectronService } from "../../shared/services/electron/electron.service";
+import { DesktopVersionsProvider } from "../../shared/services/versions/impl/desktop-versions-provider.service";
+import { VERSIONS_PROVIDER, VersionsProvider } from "../../shared/services/versions/versions-provider.interface";
 
 @Component({
 	selector: "app-guard",
@@ -10,13 +12,16 @@ import { ElectronService } from "../../shared/services/electron/electron.service
 export class DesktopPreRunGuardDialogComponent implements OnInit {
 
 	public athleteMachineId: string;
+	public releaseUrl: string;
 
 	constructor(public electronService: ElectronService,
+				@Inject(VERSIONS_PROVIDER) public versionsProvider: VersionsProvider,
 				@Inject(MAT_DIALOG_DATA) athleteMachineId: string) {
 		this.athleteMachineId = athleteMachineId;
 	}
 
 	public ngOnInit(): void {
+		this.releaseUrl = (<DesktopVersionsProvider> this.versionsProvider).getLatestReleaseUrl();
 	}
 
 	public closeApp(): void {
