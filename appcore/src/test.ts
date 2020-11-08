6e366a6da8d5c58354cc241f3a3c8fd46c5915a5
Thomas Champagne <th.champagne@gmail.com>
2020-11-08 17:59:41 +0100

    Remove jest desktop boot mock

--- appcore/src/test.ts
+++ appcore/src/test.ts
@@ -50,6 +50,3 @@ Object.defineProperty(window, "require", {
     return {};
   }
 });
-
-// Mock/bypass desktop boot for all test (to be removed)
-jest.mock("./app/app-load/desktop/desktop-boot", () => null);
