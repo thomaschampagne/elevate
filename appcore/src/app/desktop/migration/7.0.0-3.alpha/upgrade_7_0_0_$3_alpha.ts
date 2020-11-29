8e6a0b97c0113b7603528699fecb5295bda84f34
Thomas Champagne <th.champagne@gmail.com>
2020-11-29 14:12:30 +0100

    Append migration for: rename FILE_SYSTEM connector to FILE enum value

--- appcore/src/app/desktop/migration/7.0.0-3.alpha/upgrade_7_0_0_$3_alpha.ts
+++ appcore/src/app/desktop/migration/7.0.0-3.alpha/upgrade_7_0_0_$3_alpha.ts
@@ -3,9 +3,11 @@ import { DesktopMigration } from "../desktop-migrations.model";
 export class Upgrade_7_0_0_$3_alpha extends DesktopMigration {
   public version = "7.0.0-3.alpha";
 
-  public description = "Add athlete first name, last name, birth year & sports properties";
+  public description =
+    "Add athlete first name, last name, birth year & sports properties; Rename FILE_SYSTEM connector to FILE enum value";
 
   public upgrade(db: LokiConstructor): Promise<void> {
+    // Add athlete first name, last name, birth year & sports properties
     const athleteCollection = db.getCollection("athlete");
 
     if (athleteCollection) {
@@ -20,7 +22,14 @@ export class Upgrade_7_0_0_$3_alpha extends DesktopMigration {
       }
     }
 
-    // ....
-    return Promise.resolve();
+    // Rename FILE_SYSTEM connector to FILE enum value
+    const connectorSyncDateTimeCollection = db.getCollection("connectorSyncDateTime");
+    const existingFileConnector = connectorSyncDateTimeCollection.findOne({ connectorType: "FILE_SYSTEM" });
+    if (existingFileConnector) {
+      existingFileConnector.connectorType = "FILE";
+      connectorSyncDateTimeCollection.update(existingFileConnector);
+    }
+
+    return this.saveDatabase(db);
   }
 }
--- appcore/src/app/desktop/migration/desktop-migrations.model.ts
+++ appcore/src/app/desktop/migration/desktop-migrations.model.ts
@@ -4,5 +4,17 @@ export abstract class DesktopMigration {
 
   constructor() {}
 
+  protected saveDatabase(db: LokiConstructor): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      db.saveDatabase(err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
   public abstract upgrade(db: LokiConstructor): Promise<void>;
 }
