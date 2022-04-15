df51b3707e499cc7df8348797b9c92b11fd6a655
Thomas Champagne <th.champagne@gmail.com>
2022-04-15 19:29:50 +0200

    Migration to 7.0.0-beta.3

--- appcore/src/app/desktop/migration/desktop-registered-migrations.ts
+++ appcore/src/app/desktop/migration/desktop-registered-migrations.ts
@@ -1,10 +1,12 @@
 import { DesktopMigration } from "./desktop-migrations.model";
+import { Upgrade_7_0_0$beta_3 } from "./upgrade_7_0_0-beta.3/upgrade_7_0_0-beta.3";
 
 /**
  * Add desktop migrations inside LIST. The LIST is sorted from lowest to high versions
  */
 export class DesktopRegisteredMigrations {
   public static readonly LIST: DesktopMigration[] = [
+    new Upgrade_7_0_0$beta_3() // To version 7.0.0-beta.3
     /* Next Upgrade Here */
   ];
 }
