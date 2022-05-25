99f64107f991e7ac5c8308e89a0dd417d4e580ea
Thomas Champagne <th.champagne@gmail.com>
2022-05-25 09:40:08 +0200

    Bump to 7.0.0-beta.5

--- appcore/src/app/desktop/migration/desktop-registered-migrations.ts
+++ appcore/src/app/desktop/migration/desktop-registered-migrations.ts
@@ -1,12 +1,14 @@
 import { DesktopMigration } from "./desktop-migrations.model";
 import { Upgrade_7_0_0$beta_3 } from "./upgrade_7_0_0-beta.3/upgrade_7_0_0-beta.3";
+import { Upgrade_7_0_0$beta_5 } from "./upgrade_7_0_0-beta.5/upgrade_7_0_0-beta.5";
 
 /**
  * Add desktop migrations inside LIST. The LIST is sorted from lowest to high versions
  */
 export class DesktopRegisteredMigrations {
   public static readonly LIST: DesktopMigration[] = [
-    new Upgrade_7_0_0$beta_3() // To version 7.0.0-beta.3
+    new Upgrade_7_0_0$beta_3(), // To version 7.0.0-beta.3
+    new Upgrade_7_0_0$beta_5() // To version 7.0.0-beta.5
     /* Next Upgrade Here */
   ];
 }
